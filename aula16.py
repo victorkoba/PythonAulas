d6c3294b807af355caa2e6a95ca180f8cc3c638f
Victor Koba <victorkoba08@gmail.com>
2026-06-08 17:30:14 -0300

    aula16

---
+++ aula16.py
@@ -0,0 +1,25 @@
+# lanche = ('Hamburguer', 'Suco', 'Pizza', 'Refrigerante')
+#
+# print(sorted(lanche))
+
+
+# for comida in lanche:
+#     print(f'Eu vou comer {comida}')
+#
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]}')
+#
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+#
+# print('Comi pra caramba')
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c.count(5))
+print(c.index(2, 1))
+pessoa = ('Gustavo', 39, 'M')
+print(pessoa)
+del(pessoa)
+print(pessoa)
